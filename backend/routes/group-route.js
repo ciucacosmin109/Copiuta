485c884a9b52f5bb4cf6eb459412e46e0695d0be
Marius Cosmin Ciuca <ciucacosmin109@gmail.com>
2020-12-09 10:40:22 +0200

    Added group routes structure

---
+++ backend/routes/group-route.js
@@ -0,0 +1,9 @@
+const { database, models } = require("../../database");
+
+const getAllGroups = async (req, res) => {};
+const getGroup = async (req, res) => {};
+const addGroup = async (req, res) => {};
+const updateGroup = async (req, res) => {};
+const deleteGroup = async (req, res) => {};
+
+module.exports = { getAllGroups, getGroup, addGroup, updateGroup, deleteGroup };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,13 +4,20 @@ const bodyParser = require("body-parser"); //const jsonParser = bodyParser.json(
 const router = express.Router();
 
 // Include the route functions
-const linkRoute = require('./link-route');
+const linkRoute = require("./link-route");
+const groupRoute = require("./group-route");
 
 // Define the routes
-router.get('/api/link/getAll/:noteId', linkRoute.getAllLinks)
-router.get('/api/link/get/:id', linkRoute.getLink)
-router.post('/api/link/add/:noteId', linkRoute.addLink)
-router.put('/api/link/edit/:id', linkRoute.updateLink)
-router.delete('/api/link/delete/:id', linkRoute.deleteLink)
+router.get("/api/link/getAll/:noteId", linkRoute.getAllLinks);
+router.get("/api/link/get/:id", linkRoute.getLink);
+router.post("/api/link/add/:noteId", linkRoute.addLink);
+router.put("/api/link/edit/:id", linkRoute.updateLink);
+router.delete("/api/link/delete/:id", linkRoute.deleteLink);
+
+router.get("/api/group/getAll/:noteId", groupRoute.getAllGroups);
+router.get("/api/group/get/:id", groupRoute.getGroup);
+router.post("/api/group/add/:noteId", groupRoute.addGroup);
+router.put("/api/group/edit/:id", groupRoute.updateGroup);
+router.delete("/api/group/delete/:id", groupRoute.deleteGroup);
 
 module.exports = router;
