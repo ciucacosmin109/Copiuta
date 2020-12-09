b31f8a42f69b0e741295aa01d9593856bae9efa3
Marius Cosmin Ciuca <ciucacosmin109@gmail.com>
2020-12-09 10:35:37 +0200

    Removed services

--- backend/routes/link-route.js
+++ backend/routes/link-route.js
@@ -1,31 +1,25 @@
-const linkService = require('./services/link-service');
- 
+const { database, models } = require("../../database");
+
 const getAllLinks = async (req, res) => {
-    try {  
-        // console.log({
-        //     headers: req.headers, // autentificare ?
-        //     params: req.params, // parametrii cu : din rute (:id de exemplu)
-        //     query: req.query, // parametrii aia din URL cu ? si &
-        //     body: req.body // body - obiecte json
-        // });
-        
-        const links = await linkService.getAll(req.params.noteId);
-        res.status(200).send(links);
-    } catch (err) {
-        res.status(500).send( err.message );
-    }
-};
-const getLink = async (req, res) => { 
+  try {
+    // console.log({
+    //     headers: req.headers, // autentificare ?
+    //     params: req.params, // parametrii cu : din rute (:id de exemplu)
+    //     query: req.query, // parametrii aia din URL cu ? si &
+    //     body: req.body // body - obiecte json
+    // });
+    const links = await models.Link.findAll({
+      where: { NoteId: noteId },
+    });
 
+    res.status(200).send(links);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
-const addLink = async (req, res) => { 
-
-};
-const updateLink = async (req, res) => { 
-
-};
-const deleteLink = async (req, res) => { 
-
-};
+const getLink = async (req, res) => {};
+const addLink = async (req, res) => {};
+const updateLink = async (req, res) => {};
+const deleteLink = async (req, res) => {};
 
 module.exports = { getAllLinks, getLink, addLink, updateLink, deleteLink };
